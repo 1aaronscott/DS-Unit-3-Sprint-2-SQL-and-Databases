d04e1d7f4689e0d23b56c61b321f4b61f461953d
1aaronscott <1aaronscott at gmail.com>
2019-09-11 22:38:15 -0500

    modified:   module3-nosql-and-document-oriented-databases/323_DS6_Lambda_MongoDB_Playground.py

--- module3-nosql-and-document-oriented-databases/323_DS6_Lambda_MongoDB_Playground.py
+++ module3-nosql-and-document-oriented-databases/323_DS6_Lambda_MongoDB_Playground.py
@@ -24,13 +24,43 @@ client = pymongo.MongoClient(
     "mongodb://admin:password@cluster0-shard-00-00-k4s0t.mongodb.net:27017,cluster0-shard-00-01-k4s0t.mongodb.net:27017,cluster0-shard-00-02-k4s0t.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
 db = client.test
 
+# test case that adds one table
+# more_docs = []
+# for ai in armory_items:
+#     doc = {'sql_index': ai[0],
+#            'name': ai[1],
+#            'value': ai[2],
+#            'weight': ai[3]}
+#     more_docs.append(doc)
+
+# db.test.insert_many(more_docs)
+# list(db.test.find())
+
 more_docs = []
-for ai in armory_items:
-    doc = {'sql_index': ai[0],
-           'name': ai[1],
-           'value': ai[2],
-           'weight': ai[3]}
-    more_docs.append(doc)
+table_set = set()
+for r in range(0, len(sl_curs.execute('SELECT name from sqlite_master where type= "table";')
+                      .fetchall())):
+    for c in range(0, len(sl_curs.execute('SELECT name from sqlite_master where type= "table";')
+                          .fetchall()[r])):
+        string = sl_curs.execute(
+            'SELECT name from sqlite_master where type= "table";').fetchall()[r][0]
+        table_set.add(string)
+table_list = list(table_set)
+
+for cur_table in table_list:
+    col_list = []
+    # generates a list of columns in current table; assumes second column is the column name
+    for r in range(0, len(sl_curs.execute(f'PRAGMA table_info({cur_table});').fetchall())):
+        for c in range(0, len(sl_curs.execute(f'PRAGMA table_info({cur_table});').fetchall()[r])):
+            string = sl_curs.execute(f'PRAGMA table_info({cur_table});').fetchall()[r][1]
+            if string not in col_list:
+                col_list.append(string)
+    rows = sl_curs.execute(f'SELECT * FROM {cur_table};').fetchall()
+    for row in rows:
+        for col in col_list:
+            doc = {'table': cur_table, 'feature': col,
+                   'value': row[col_list.index(col)]}
+            more_docs.append(doc)
 
 db.test.insert_many(more_docs)
 list(db.test.find())
